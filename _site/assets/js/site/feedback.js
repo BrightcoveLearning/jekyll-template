02eda642f92727bf9c9fb452b915a66367736d59
Robert Crooks <rcrooks@brightcove.com>
2019-12-06 14:15:18 -0500

    wip

--- _site/assets/js/site/feedback.js
+++ _site/assets/js/site/feedback.js
@@ -1,5 +1,5 @@
 var fby = fby || [];
-fby.push(['showTab', {id: '14941', position: 'right', color: '#F3951D'}]);
+fby.push(['showTab', {id: '14941', position: 'right', color: 'rgb(5, 155, 179)'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
